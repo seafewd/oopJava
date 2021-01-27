51a7c90091c03c1b97eaefd1bd9734a4a598f507
Alex <alexander.korpas@protonmail.com>
2021-01-27 21:45:10 +0100

    more tests

--- test/java/CarTest.java
+++ test/java/CarTest.java
@@ -1,10 +1,14 @@
+import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.awt.*;
+
+import static org.junit.Assert.assertEquals;
 
 class CarTest {
+    private final Saab95 saab = new Saab95();
 
     @BeforeEach
     void setUp() {
@@ -12,6 +16,75 @@ class CarTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    /**
+     * Test method getNrDoors
+     */
+    @Test
+    public void testGetNrDoors(){
+        Assertions.assertEquals(2, saab.getNrDoors());
+    }
+
+    /**
+     * Test method getEnginePower
+     */
+    @Test
+    public void testGetEnginePower(){
+        Assertions.assertEquals(125, saab.getEnginePower());
+    }
+
+    /**
+     * Test method getCurrentSpeed
+     */
+    @Test
+    public void testGetCurrentSpeed() {
+        Assertions.assertEquals(0, saab.getCurrentSpeed());
+    }
+
+    /**
+     * Test method getColor
+     */
+    @Test
+    public void testGetColor() {
+        Assertions.assertEquals(Color.red, saab.getColor());
+    }
+
+    /**
+     * Test method setColor
+     */
+    @Test
+    public void testSetColor() {
+        saab.setColor(Color.blue);
+        Assertions.assertSame(saab.getColor(), Color.blue);
+    }
+
+    /**
+     * Test method startEngine
+     */
+    @Test
+    public void testStartEngine() {
+        saab.startEngine();
+        Assertions.assertEquals(0.1, saab.getCurrentSpeed());
+    }
+
+    /**
+     * Test method stopEngine
+     */
+    @Test
+    public void testStopEngine() {
+        saab.stopEngine();
+        Assertions.assertEquals(0, saab.getCurrentSpeed());
+    }
+
+    /**
+     * Test method getPosition
+     */
+    @Test
+    public void testGetPosition() {
+        saab.xPos = 6;
+        saab.yPos = 4;
+        Assertions.assertEquals("Current position: (4, 6)", saab.getPosition(saab));
     }
 
 }
---
+++ test/java/SaabTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SaabTest {
+    private final Saab95 saab = new Saab95();
+
+
+}
