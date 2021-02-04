012d4f20132766fa19c7970585b2d9308b4b8e14
rob <dauda@dauda>
2021-02-04 14:05:57 +0100

    Added Car class

--- src/Car.java
+++ src/Car.java
@@ -13,5 +13,6 @@ public abstract class Car extends Vehicle{
      */
     public Car(int nrDoors, double enginePower, double currentSpeed, Color color, String modelName, int weight) {
         super(nrDoors, enginePower, currentSpeed, color, modelName, weight);
+        
     }
 }
