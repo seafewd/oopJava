6b45121c4c2961b9eac9a9fafde826ac15651e7d
Alex <alexander.korpas@protonmail.com>
2021-02-17 13:34:02 +0100

    startcars + stopcars

--- src/CarView.java
+++ src/CarView.java
@@ -124,6 +124,13 @@ public class CarView extends JFrame{
             }
         });
 
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                carC.startAllCars(gasAmount);
+            }
+        });
+
         // Make the frame pack all it's components by respecting the sizes if possible.
         this.pack();
 
