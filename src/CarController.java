717980c9d4ab0cbb4c83cb7fee4ba30f5a6944c2
Alex <alexander.korpas@protonmail.com>
2021-02-17 15:15:48 +0100

    tetheh

--- src/CarController.java
+++ src/CarController.java
@@ -33,8 +33,8 @@ public class CarController {
         CarController cc = new CarController();
 
         cc.cars.add(new Volvo240(0,0));
-        cc.cars.add(new Saab95(0, 100));
-        cc.cars.add(new Scania(0,200));
+        cc.cars.add(new Saab95(0, 10));
+        cc.cars.add(new Scania(0,20));
 
         // Start a new view and send a reference of self
         cc.frame = new CarView("CarSim 1.0", cc);
